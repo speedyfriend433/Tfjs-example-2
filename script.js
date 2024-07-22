b0f5770211780dd3ee5a43b3bcedb3e3bb0a6582
speedyfriend433 <speedyfriend13@gmail.com>
2024-07-22 11:06:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 import * as tf from '@tensorflow/tfjs';
 let chart;
 
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 10, activation: 'relu', inputShape: [1]}));
-model.add(tf.layers.dense({units: 1}));
-
-model.compile({
-    optimizer: 'adam',
-    loss: 'meanSquaredError'
-});
-
-model.fit(xs, ys, {epochs: 200}).then(() => {
-    drawChart();
-});
-
-function makePrediction() {
-    const inputValue = document.getElementById('inputValue').value;
-    const prediction = model.predict(tf.tensor2d([parseFloat(inputValue)], [1, 1]));
-    prediction.array().then(preds => {
-        document.getElementById('predictionResult').innerText = `Predicted value: ${preds[0][0].toFixed(2)}`;
+async function trainAndPredict() {
+    const rawData = document.getElementById('inputData').value;
+    const data = rawData.trim().split('\n').map(row => row.split(',').map(Number));
+    const xs = data.map(row => row.slice(0, -1));
+    const ys = data.map(row => row.slice(-1));
+    const inputTensor = tf.tensor2d(xs);
+    const outputTensor = tf.tensor2d(ys);
+    const model = tf.sequential();
+    
+    model.add(tf.layers.dense({units: 10, activation: 'relu', inputShape: [xs[0].length]}));
+    model.add(tf.layers.dense({units: 1}));
+    model.compile({
+        optimizer: 'adam',
+        loss: 'meanSquaredError'
     });
-}
 
-async function drawChart() {
-    const predictions = model.predict(tf.tensor2d([1, 2, 3, 4, 5, 6], [6, 1]));
+    await model.fit(inputTensor, outputTensor, {epochs: 200});
+
+    const predictions = model.predict(inputTensor);
     const preds = await predictions.array();
 
+    updateChart(xs, ys, preds);
+}
+
+function updateChart(xs, ys, preds) {
     const ctx = document.getElementById('myChart').getContext('2d');
-    
+
     if (chart) {
         chart.destroy();
     }
@@ -37,16 +35,16 @@ async function drawChart() {
     chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: [1, 2, 3, 4, 5, 6],
+            labels: xs.map((_, i) => i + 1),
             datasets: [{
+                label: 'Actual values',
+                data: ys.map(y => y[0]),
+                borderColor: 'red',
+                fill: false
+            }, {
                 label: 'Predicted values',
                 data: preds.map(p => p[0]),
                 borderColor: 'blue',
-                fill: false
-            }, {
-                label: 'Original values',
-                data: [1, 3, 5, 7],
-                borderColor: 'red',
                 fill: false
             }]
         },
